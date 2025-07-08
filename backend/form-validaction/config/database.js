f26fafe3b9724bf7f3d272f2ee54dc8be8728098
Hareschaudhary <hareschaudhary34@gmail.com>
2025-07-08 22:43:46 +0530

    Updated .gitignore and removed tracked files that should be ignored

--- backend/form-validaction/config/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const DataBaseConact =()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/form-data").then(()=>{
-        console.log("data base conacted")
-    })
-}
